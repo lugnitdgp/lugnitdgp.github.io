392b2324350d0eac99ee39afc0e91c3294778c4e
SingularReza <chandandharana@gmail.com>
2018-08-05 20:19:17 +0530

    added some css

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,15 +1,14 @@
 <template lang="html">
   <div class="container">
     <div class="row">
-      <div class="col" v-for="event in events" :key="event.name">
+      <div class="col-lg-4" v-for="event in events" :key="event.name">
         <div id="image">
           <img src="../assets/logo.png"/>
-          <span id="date">{{event.date}}</span>
+          <div id="date">{{event.date}}&nbsp;<img src="../assets/calendar.png" id="calendar"/></div>
         </div>
         <div id="info">
-          <h4>{{event.name}}</h4>
-          <span>{{event.time}}</span>
-          <span>{{event.venue}}</span>
+          <h4 class="info-head">{{event.name}}</h4>
+          <span class="info-head"><img src="../assets/clock.png" class="icons"/>&nbsp;&nbsp;{{event.time}}&nbsp;<img src="../assets/location.png" class="icons"/>&nbsp;&nbsp;{{event.venue}}</span>
           <div id="text">{{event.text}}</div>
         </div>
       </div>
@@ -26,21 +25,21 @@ export default {
       events: [
         {
           date: '12-2-2018',
-          name: 'event',
+          name: 'Some Event',
           time: '20:00 AM',
           venue: 'dm sen audi',
           text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit ratione dolorem, reprehenderit laboriosam eius tenetur modi quas, esse nesciunt similique repellendus nulla.'
         },
         {
           date: '12-2-2018',
-          name: 'event1',
+          name: 'Some Event1',
           time: '20:00 AM',
           venue: 'dm sen audi',
           text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit ratione dolorem, reprehenderit laboriosam eius tenetur modi quas, esse nesciunt similique repellendus nulla.'
         },
         {
           date: '12-2-2018',
-          name: 'event2',
+          name: 'Some Event2',
           time: '20:00 AM',
           venue: 'dm sen audi',
           text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit ratione dolorem, reprehenderit laboriosam eius tenetur modi quas, esse nesciunt similique repellendus nulla.'
@@ -52,18 +51,41 @@ export default {
 </script>
 
 <style lang="css">
-  div, span {
+  #image {
     border: 1px solid black;
     position:relative;
-  }
-
-  .container {
-    width: 33vw;
   }
 
   #date {
     position: absolute;
     left: 0%;
     bottom: 10%;
+    border: 1px solid black;
+    border-left: none;
+    height:3.6vh;
+  }
+
+  #calendar{
+    height: 3vh;
+    border-left: 1px solid grey;
+    padding: .3vh;
+  }
+
+  .icons {
+    height: 2vh;
+    border-left: 1px solid grey;
+     padding: .3vh;
+  }
+
+  .container {
+  }
+
+  .info-head {
+    display: block;
+    text-align: left;
+  }
+
+  #text {
+    text-align: left;
   }
 </style>
