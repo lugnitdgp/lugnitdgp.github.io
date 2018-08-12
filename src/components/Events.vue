f5664706833d092db81fb3e259da0c0c6ca88542
SingularReza <chandandharana@gmail.com>
2018-08-12 18:26:28 +0530

    completed card carousel

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,24 +1,31 @@
 <template lang="html">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-4" v-for="event in events" :key="event.name">
-        <div id="image">
-          <img src="../assets/logo.png"/>
-          <div id="date">{{event.date}}&nbsp;<img src="../assets/calendar.png" id="calendar"/></div>
+  <div class="container-fluid">
+    <div id="test" class="col-md-4">
+      <flickity ref="flickity" :options="flickityOptions">
+        <div class="carousel-cell" v-for="event in events" :key="event.name">
+          <div id="image">
+            <img src="../assets/logo.png"/>
+            <div id="date">{{event.date}}&nbsp;<img src="../assets/calendar.png" id="calendar"/></div>
+          </div>
+          <div id="info">
+            <h4 class="info-head">{{event.name}}</h4>
+            <span class="info-head"><img src="../assets/clock.png" class="icons"/>&nbsp;&nbsp;{{event.time}}&nbsp;<img src="../assets/location.png" class="icons"/>&nbsp;&nbsp;{{event.venue}}</span>
+            <div id="text">{{event.text}}</div>
+          </div>
         </div>
-        <div id="info">
-          <h4 class="info-head">{{event.name}}</h4>
-          <span class="info-head"><img src="../assets/clock.png" class="icons"/>&nbsp;&nbsp;{{event.time}}&nbsp;<img src="../assets/location.png" class="icons"/>&nbsp;&nbsp;{{event.venue}}</span>
-          <div id="text">{{event.text}}</div>
-        </div>
-      </div>
+      </flickity>
     </div>
   </div>
 </template>
 
 <script>
+
+import Flickity from 'vue-flickity'
+
 export default {
   name: 'Events',
+
+  components: { Flickity },
 
   data () {
     return {
@@ -44,13 +51,38 @@ export default {
           venue: 'dm sen audi',
           text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit ratione dolorem, reprehenderit laboriosam eius tenetur modi quas, esse nesciunt similique repellendus nulla.'
         }
-      ]
+      ],
+
+      flickityOptions: {
+        initialIndex: 1,
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: true,
+        autoplay: true,
+        cellAlign: 'center'
+      }
+    }
+  },
+
+  methods: {
+    next () {
+      this.$refs.flickity.next()
+    },
+
+    prev () {
+      this.$refs.flickity.prev()
     }
   }
 }
 </script>
 
 <style lang="css">
+
+  .carousel-cell {
+    width:100%;
+    margin:2%;
+  }
+
   #image {
     border: 1px solid black;
     position:relative;
@@ -77,12 +109,11 @@ export default {
      padding: .3vh;
   }
 
-  .container {
-  }
-
   .info-head {
     display: block;
     text-align: left;
+    margin:2%;
+    margin-left:0%;
   }
 
   #text {
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-<<<<<<< HEAD
-=======
-import Events from '@/components/Events'
->>>>>>> cefa8175d7bb64a99dbc0de961c7feef9c7e57b0
 
 Vue.use(Router)
 
@@ -14,14 +10,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-<<<<<<< HEAD
-=======
-    },
-    {
-      path: '/events',
-      name: 'Events',
-      component: Events
->>>>>>> cefa8175d7bb64a99dbc0de961c7feef9c7e57b0
     }
   ]
 })
