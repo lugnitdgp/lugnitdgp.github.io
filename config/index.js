e2fc49d5e4778d737bcc7625a30ee2e5ac49c025
MTnes <mayank.nitdgp@gmail.com>
2018-08-13 12:22:09 +0530

    Change config/index.js

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/lugnitdgp-website/',
 
     /**
      * Source Maps
