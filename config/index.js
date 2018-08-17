d92064bf473f10224f5f91d1366383ab9452c4c3
SingularReza <36134253+SingularReza@users.noreply.github.com>
2018-08-17 14:58:37 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'lugnitdgp-website/',
     proxyTable: {},
 
     // Various Dev Server settings
