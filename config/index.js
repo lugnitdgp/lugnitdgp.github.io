2f4ccd2f8f142807f4911aea9f3196f8dbf58b89
SingularReza <chandandharana@gmail.com>
2018-08-17 15:18:44 +0530

    ran npm deploy

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'lugnitgp-website/',
     proxyTable: {},
 
     // Various Dev Server settings
