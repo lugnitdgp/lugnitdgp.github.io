09199d3cff8364ee4fb40d0b31971ac88e8c143f
Ankan Poddar <friendyankan@gmail.com>
2018-08-18 21:44:21 +0530

    Add linting rules and solve existing linting issues

--- src/components/AnimateNumber.vue
+++ src/components/AnimateNumber.vue
@@ -6,6 +6,7 @@
 export default {
   props: {
     'number': {
+      type: Number,
       default: 0
     }
   },
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home">
-    <about-us/>
+    <about-us />
   </div>
 </template>
 
