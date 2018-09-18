8dd58ed2f6b09d2a89b1494d69803bf34c200609
arc9693 <archana.choudhary.9693@gmail.com>
2018-09-18 16:02:57 +0530

    Edit contact section and footer

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="container-fluid Contact-us">
-    <div class="row p-0">
-      <div class="col-md-12 col-lg-6 p-0 m-0 pb-lg-5">
-
-        <h2 class="text-center font-weight-bold pt-5 pb-3">G<span class="highlighted">E</span>T IN T<span class="highlighted">UC</span>H</h2>
+  <div class="container-fluid">
+    <h2 class="text-center font-weight-bold pt-5 pb-3">G<span class="highlighted">E</span>T IN TO<span class="highlighted">UC</span>H</h2>
+    <div class="row p-0 Contact-us">
+      <div class="col-md-12 col-lg-5 m-0 pb-4 pt-4">
         <div class="row d-flex justify-content-center">
           <div class="col-11 font-weight-bold ">
             <div class="contact-card">
               Devansh Goenka(President) <br>
               <i class="fas fa-phone" /> +91 96748 25450 <br>
               <i class="fas fa-envelope" /> devanshgoenka97@gmail.com <br>
+            </div>
+          </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+          <div class="col-11 font-weight-bold ">
+            <div class="contact-card">
+              Arnav Kumar (Vice President) <br>
+              <i class="fas fa-phone" /> +91 79038 65654 <br>
+              <i class="fas fa-envelope" /> arnav.kr.grv@gmail.com <br>
             </div>
           </div>
         </div>
@@ -23,20 +31,7 @@
           </div>
         </div>
       </div>
-      <div class="col-sm-12 col-lg-1 links d-flex align-center">
-        <div class="row">
-          <div class="col col-lg-12">
-            <i class="fab fa-lg fa-facebook-f" />
-          </div>
-          <div class="col col-lg-12">
-            <i class="fab fa-lg fa-gitter" />
-          </div>
-          <div class="col col-lg-12">
-            <i class="fab fa-md fa-youtube" />
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-12 col-lg-5 p-0">
+      <div class="col-sm-12 col-lg-7 p-0">
         <div class="Contact-footer--right" style="background-image: url('static/images/image4.jpeg')">
           <div class="Contact-footer--right-content">
             <div class="row mt-2">
@@ -44,7 +39,7 @@
                 <i class="fas fa-map-marker-alt fa-2x" />
               </div>
               <div class="col-10">
-                <p> <strong>GNU Linux user's group<br>  NIT Durgapur</strong></p><p> Mahatma Gandhi Rd, A-Zone, Durgapur<br>West Bengal 713209</p>
+                <p> <strong>GNU Linux user's group<br>  NIT Durgapur</strong></p><p>Durgapur<br>West Bengal 713209</p>
               </div>
             </div>
           </div>
@@ -89,7 +84,7 @@ export default {
 }
 .Contact-footer--right .Contact-footer--right-content > div {
   position: relative;
-  max-width: 70%;
+  max-width: 22rem;
   padding: 36px 36px 36px 36px;
   margin: auto;
   background: #444;
@@ -109,14 +104,5 @@ export default {
   border-color: rgba(218,77,60,0);
   border-top-color: #444;
 }
-.links i{
-  border:2px solid #444;
-  padding: 0.6rem;
-  margin: 1rem;
-}
-.links i:hover{
-  border:2px solid #fff;
-  background-color: #ff9800;
-  color:white;
-}
+
 </style>
