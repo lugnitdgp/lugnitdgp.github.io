fc36a59f01184db785134822bc16955b9420560e
arc9693 <archana.choudhary.9693@gmail.com>
2018-10-02 00:44:35 +0530

    Modify BlogSection
    This fixes inconsistency in height of blog post cards in BlogSection on homepage.
    
    Fixes #15

--- src/components/BlogSection.vue
+++ src/components/BlogSection.vue
@@ -1,56 +1,47 @@
 <template>
   <div class="container blog-section-container">
     <h2 class="text-center font-weight-bold pt-5 pb-3">Our B<span class="highlighted">lo</span>g</h2>
-    <div class="row align-items-center">
-      <div
-        v-for="(item, index) in blog"
-        :key="index"
-        class="col-10 offset-1 col-md-6 offset-md-0 col-lg-4"
+    <div class="row align-middle d-flex flex-wrap">
+      <div v-for="(item, index) in blog"
+           :key="index"
+           class="col-12 col-md-6 col-lg-4 mb-2"
       >
-        <v-card class="card">
-          <v-card-media
-            :src="item.thumbnail_image"
-            height="200px"
-          />
-
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-2">{{ item.title }}</h3>
-              <p class="card-text" v-html="item.content_body" />
+        <div class="card" style="width:100%;height:100%">
+          <img class="card-img-top" :src="item.thumbnail_image" alt="Card image cap" height="200px">
+          <div class="card-body">
+            <h4 class="card-title font-weight-bold">{{ item.title }}</h4>
+            <div class="row">
+              <router-link :to="{ name: 'Post' , params: { post: ConvertToKebabCase(item.title)} , query: {id : item.id}}"><v-btn flat color="orange" class="m-0">Read More</v-btn></router-link>
+              <v-spacer />
+              <social-sharing
+                url="https://vuejs.org/"
+                :title="item.title"
+                :description="item.description"
+                :hashtags="item.keywords || null"
+                inline-template
+                class="d-inline-flex justify-content-between social-share-container align-items-center"
+              >
+                <div>
+                  <network network="facebook">
+                    <i class="fab fa-facebook-f d-inline-flex social-share-icons justify-content-center align-items-center" />
+                  </network>
+                  <network network="linkedin">
+                    <i class="fab fa-linkedin d-inline-flex social-share-icons justify-content-center align-items-center" />
+                  </network>
+                  <network network="reddit">
+                    <i class="fab fa-reddit d-inline-flex social-share-icons justify-content-center align-items-center" />
+                  </network>
+                  <network network="twitter">
+                    <i class="fab fa-twitter d-inline-flex social-share-icons justify-content-center align-items-center" />
+                  </network>
+                </div>
+              </social-sharing>
             </div>
-          </v-card-title>
 
-          <v-card-actions class="justify-content-between">
-            <v-btn flat color="orange">Read More</v-btn>  <!-- Add redirection to blog link -->
-            <social-sharing
-              url="https://vuejs.org/"
-              :title="item.title"
-              :description="item.description"
-              :hashtags="item.keywords || null"
-              inline-template
-              class="d-inline-flex justify-content-between social-share-container align-items-center"
-            >  <!-- Replace url by the actual blog link -->
-              <div>
-                <network network="facebook">
-                  <i class="fab fa-facebook-f d-inline-flex social-share-icons justify-content-center align-items-center" />
-                </network>
-                <network network="linkedin">
-                  <i class="fab fa-linkedin d-inline-flex social-share-icons justify-content-center align-items-center" />
-                </network>
-                <network network="reddit">
-                  <i class="fab fa-reddit d-inline-flex social-share-icons justify-content-center align-items-center" />
-                </network>
-                <network network="twitter">
-                  <i class="fab fa-twitter d-inline-flex social-share-icons justify-content-center align-items-center" />
-                </network>
-              </div>
-            </social-sharing>
-          </v-card-actions>
-
-        </v-card>
+          </div>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -62,6 +53,13 @@ export default {
     'blog': {
       type: Array,
       default: null
+    }
+  },
+  methods: {
+    ConvertToKebabCase (string) {
+      return string.replace(/([a-z])([A-Z])/g, '$1-$2')
+        .replace(/\s+/g, '-')
+        .toLowerCase()
     }
   }
 }
