e04f308fb937aa394a2e6b53f2b37427c98780e2
SucharuRai <raisucharugzp@gmail.com>
2019-10-04 23:28:26 +0530

    Timeline feature p3 refined

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -10,7 +10,6 @@
           :key="index"
           class="col-12 mx-auto"
         >
-          <!-- class="col-12 mx-auto" :style="[(index % 2 == 0) ? float:left : float:right ]" -->
           <v-card class="containerWithin" :class="[index == active ? 'active' : '', active != null && index != active ? 'inactive' : '', 'card']">
             <v-card-title primary-title>
               <div>
@@ -120,11 +119,7 @@ export default {
         Str = mon + ',' + YearVal
         return Str
       }
-    }/*,
-    sorting (value, Str) {                    //SORTING THE DATE  Used "underscore.js" Not complete though.. Not SURE HOW TOO
-      var val = moment(String(value)).format('YYYY-MM-DD')
-      _.sortBy(val)
-    }*/
+    }                 // Sorting Done in the BACKEND itself :)
   }
 }
 </script>
