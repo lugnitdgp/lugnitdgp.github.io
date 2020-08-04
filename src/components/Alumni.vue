338a2100f610da6e9966833610cae666f89f5bd5
AbhinavKrishna26 <abhinavkrishna26chemistry@gmail.com>
2020-08-04 17:18:02 +0530

    alumni styled and categorized acc to year

--- src/components/Alumni.vue
+++ src/components/Alumni.vue
@@ -6,25 +6,23 @@
   <p class="text-center font-italic">
     "If we have seen further, it is by standing on the shoulders of giants"
   </p>
- 
+<div v-for="EachYear in Allyears" :key="EachYear.name">
 <div class="container">
 <div class="card bg-light">
- <div class="row flex-wrap justify-content-center card-body ">
-    <div v-for="alumni in profiles" :key="alumni.id">
-    <div class="container-fluid">
+     <div class="card-header p-2 pl-3">
+            <h6 class="font-weight-bold text-uppercase mb-0">
+              {{ EachYear.name }}-Graduates
+            </h6>
+          </div>
           <div class="card-group">
-    <div class="row flex-wrap justify-content-center card-body">
-          <div class="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
-         <div data-toggle="tooltip" :title="alumni.first_name">
-    
-             <div data-toggle="modal" :data-target="'#user'+alumni.id" class="Profile-avatar m-2 m-lg-4 " :style="{'background-image':'url('+alumni.image+')'}" />
+          <div class="row flex-wrap justify-content-center card-body">
+            <div v-for="alumni in EachYear.members" :key="alumni.id">
+             <div class="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
+              <div data-toggle="tooltip" :title="alumni.first_name">
+                <div data-toggle="modal" :data-target="'#user'+alumni.id" class="Profile-avatar m-2 m-lg-4 " :style="{'background-image':'url('+alumni.image+')'}" />
+              </div> 
               <h4 class="font-weight-bold dark-grey-text my-4">  {{ alumni.first_name }}<br /> <span class="highlighted"> {{ alumni.last_name }}</span></h4>
-              Passout Year-<span class="highlighted"> {{ alumni.passout_year }}</span>
-              
-  </div>
-         </div>
-     </div>
-          <div :id="'user'+alumni.id" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+              <div :id="'user'+alumni.id" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered ">
                   <div class="modal-body">
                     <div class="Profile p-0 m-0">
@@ -45,7 +43,7 @@
                           </blockquote>
                         </div>
                       </div>
-                        <div class="row">
+                      <div class="row">
                         <div class="col-12 text-center">
                           <div class="Profile-links fabs">
                             <a v-if="alumni.facebook_link != null" target="_blank" :href="alumni.facebook_link"><i class="fab fa-lg fa-facebook-f" /></a>
@@ -55,18 +53,20 @@
                             <a v-if="alumni.email != null" target="_blank" :href="'mailto:'+alumni.email"><i class="fa fa-lg fa-envelope" /></a>
                           </div>
                         </div>
+                      </div>
                     </div>
-                   </div>
                   </div>
-                  </div>
-                 </div>
                 </div>
-        </div> 
-   </div>
-  </div>
-  </div>
+            </div>
 </div>
-    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+   
 </section>
 </template>
 
@@ -75,16 +75,18 @@ import common from '@/services/common.js'
 export default {
   data () {
     return {
-      profiles: []
+      Allyears: [{name: '2020', members: []},
+        {name: '2019', members: []}]
     }
   },
   created () {
     common.getAlumni()
       .then(response => {
-        this.profiles = response.data
-        console.log(response.data)
-        this.profiles.forEach((member) => {
+        const profiles = response.data
+        console.log(profiles)
+        profiles.forEach((member) => {
           if (member.image === null) { member.image = 'static/images/Linux-Avatar.png' }
+          this.categorise(member)
         })
         this.$emit('hideloader', true)
         this.$emit('hideloader', true)
@@ -93,6 +95,13 @@ export default {
       .catch(e => {
         console.log(e)
       })
+  },
+  methods: {
+    categorise (member) {
+      if (member.passout_year === 2020) {
+        this.Allyears[0].members.push(member) 
+      } else if (member.passout_year === 2019) { this.Allyears[1].members.push(member) } 
+    }
   }
 }
 </script>
