387d06c514cd86b0ddc41661a1ccda1c6b92dd8b
YASHMAROO <marooyash@yahoo.in>
2020-08-20 18:55:42 +0530

    Added featured events carousel to events page,displaying featured blogs and events on the home page

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,8 +1,60 @@
 <template>
   <div id="events">
+    <div id="eventCarousel" class="carousel slide" data-ride="carousel" data-interval="2000">
+      <h1 class="text-center font-weight-bold pt-5 pb-50 featured">
+        OUR <span class="highlighted">FEATURED</span>  EVENTS
+      </h1>
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li
+          v-for="(item, index) in featuredEvents"
+          :key="index"
+          data-target="#eventCarousel"
+          :data-slide-to="index"
+          :class="[index == 0 ? 'active' : '']"
+        />
+      </ol>
+      
+      <!-- Carousel items -->
+      <div class="carousel-inner">
+        <div
+          v-for="(item, index) in featuredEvents"
+          :key="index"
+          :class="[index == 0 ? 'active' : '', 'carousel-item']"
+        >
+          <div class="row">
+            <div class="col-12 col-lg-6 col-md-6 col-sm-6">
+              <v-card class="card-container centrf">
+                <img :src="item.event_image" class="responsive">    
+              </v-card>
+            </div>
+            <div class="col-12 col-lg-6 col-md-6 col-sm-6">
+              <v-card class="caption-container">
+                <div class="date">
+                  <span>
+                    <span>{{ displayheading(new Date(item.event_timing).toLocaleDateString('en-US', {day: '2-digit', month: 'long', year: 'numeric'})).first }}</span><span class="highlighted">{{ displayheading(new Date(item.event_timing).toLocaleDateString('en-US', {day: '2-digit', month: 'long', year: 'numeric'})).second }}</span>
+                    <i class="far fa-calendar-alt" aria-hidden="true" />
+                  </span>
+                </div>
+                <div class="responsive-text" v-html="item.description" />
+              </v-card>
+            </div>
+          </div>    
+        </div>
+      </div>
+
+      <!-- Control buttons -->
+      <a href="#eventCarousel" data-slide="prev" class="carousel-control-prev">
+        <span class="carousel-control-prev-icon icon" />
+      </a>
+      <a href="#eventCarousel" data-slide="next" class="carousel-control-next">
+        <span class="carousel-control-next-icon icon" />
+      </a>
+    </div>
+
     <div class="container event-section-container">
-      <h2 class="text-center font-weight-bold pt-5 pb-5">
-        Our <span class="highlighted">Eve</span>nts
+      <h2 class="text-center font-weight-bold pt-5 pb-50">
+        Our E<span class="highlighted">ven</span>ts
       </h2>
       <div :class="[active != null ? 'active' : '', 'row align-items-center']">
         <div
@@ -37,7 +89,6 @@
                 <div class="card-text" v-html="item.description" />
               </div>
             </v-card-title>
-
             <v-card-actions class="justify-content-between">
               <v-btn v-if="active != index" flat color="orange" @click="active = index">
                 Read More
@@ -55,12 +106,12 @@
 
 <script>
 import common from '@/services/common.js'
-
 export default {
   name: 'Events',
   data () {
     return {
       events: [],
+      featuredEvents: [],
       active: null
     }
   },
@@ -68,6 +119,7 @@ export default {
     common.getEvents()
       .then(response => {
         this.events = response.data
+        this.categorise(this.events)
         this.$emit('hideloader', true)
       })
       .catch(e => {
@@ -81,29 +133,187 @@ export default {
         first: text.substr(0, 3),
         second: text.substr(3, length)
       }
+    },
+    categorise (events) {
+      events.forEach(event => {
+        if (event.featured) {
+          this.featuredEvents.push(event)
+        } 
+      })
+      console.log(this.featuredEvents)
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+#eventCarousel {
+  z-index: 1000;
+}
+.centrf{
+  display: flex !important;
+  justify-content: center;
+  align-items:center;
+}
+.caption-container {
+  display: flex !important;
+  justify-content: center;
+  align-items:center;
+  flex-direction:column;
+}
+.featured {
+  position: absolute;
+  z-index: 1000;
+  margin-left: 40%;
+  margin-top: 2%;
+}
+.carousel-item {
+  height: 100vh;
+  background-size: cover !important;
+  background-repeat: no-repeat !important;
+}
+.card-container {
+  background-color: #f7f1e3 !important;
+}
+.responsive {
+  padding-top: 150px;
+  padding-bottom:150px;
+  max-width: 90%;
+  height:100vh;
+  margin:0 auto;
+  text-align:center;
+  object-fit: contain;
+  background-size: cover !important;
+  background-repeat: no-repeat !important;
+  background-position: center;
+}
+.date {
+  padding-top: 25%;
+  font-size: 2em;
+  text-align: center;
+  min-width:100%;
+}
+.responsive-text {
+  padding: 8% 16%;
+  font-size: 1.6em;
+  min-width: 100%;
+  height: 65vh;
+  text-align: left;
+  overflow: scroll;
+}
+.caption-container {
+  background-color: #f7f1e3 !important;
+  box-shadow: none;
+  height: 100%;
+}
+.col-lg-6 {
+  padding: 0 0;
+  margin: 0 0;
+}
+.image-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-color: #000;
+  opacity: 0.4;
+}
+.carousel-control-prev {
+  left: -4%;
+}
+.carousel-control-next {
+  right: -4%;
+}
+.carousel-control-next-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E");
+}
+@media only screen and (max-width: 576px) {
+  .featured {
+    margin-top: 0;
+    margin-left: 5%;
+    display: none;
+  }
+  
+  .responsive {
+    height: 50vh;
+    padding-top:70px !important;
+    padding-bottom:0px !important;
+  }
+  .date {
+    padding-top: 2%;
+    font-size: 1.5em;
+  }
+  .responsive-text {
+    height: 45vh;
+    padding: 2% 16%;
+    font-size: 1.2em;
+    text-align: center;
+  }
+  .carousel-control-prev-icon {
+      background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E");
+  }
+}
+@media only screen and (min-width: 576px) and (max-width: 768px) {
+  .featured {
+    margin-top: 7%;
+    margin-left: 20%;
+  }
+  .date{
+    padding-top: 45%;
+  }
+  .responsive {
+    padding-bottom:0px !important;
+    padding-top:80px;
+  }
+  .responsive-text {
+    padding: 8% 16%;
+    font-size: 1.1em;
+    text-align: left;
+    height: 60vh;
+  }
+}
+@media only screen and (min-width: 768px) and (max-width: 992px) {
+  .featured {
+    margin-top: 6%;
+    margin-left: 25%;
+  }
+  .date{
+    padding-top: 38%;
+  }
+  .responsive-text {
+    padding: 8% 16%;
+    font-size: 1.2em;
+    text-align: left;
+    height: 60vh;
+  }
+}
+@media only screen and (min-width: 992px) and (max-width: 1200px) {
+  .featured {
+    margin-top: 3%;
+    margin-left: 30%;
+  }
+  .date{
+    padding-top: 39%;
+  }
+  .responsive-text {
+    padding: 5% 16%;
+    font-size: 1.4em;
+    text-align: left;
+    height: 60vh;
+  }
+}
 h2 {
   margin-top: 40px;
   text-transform: uppercase;
 }
-
 span.highlighted {
   color: #fa631c;
 }
-
 .card {
   margin-bottom: 20px;
   transition: all 0.4s ease-in-out;
-
   .cover {
     background: rgba(255, 255, 255, 0.2);
     width: 100%;
-
     & > span {
       position: absolute;
       background: rgba(255, 255, 255, 0.75);
@@ -111,13 +321,11 @@ span.highlighted {
       height: 40px;
       line-height: 40px;
       padding: 0 0 0 10px;
-
       font-family: "Roboto", sans-serif;
       font-size: 18px;
       font-weight: bold;
       letter-spacing: 0.7px;
       color: #9c9c9c;
-
       i {
         font-size: 21px;
         line-height: 40px;
@@ -129,7 +337,6 @@ span.highlighted {
       }
     }
   }
-
   .card-text {
     font-size: 1rem;
     height: 42px;
@@ -137,7 +344,6 @@ span.highlighted {
     text-overflow: ellipsis;
     color: rgb(156, 156, 156);
   }
-
   i {
     font-size: 18px;
     margin-right: 3px;
@@ -148,69 +354,56 @@ span.highlighted {
   i.fa-map-marker-alt {
     color: #062657;
   }
-
   .links {
     line-height: 35px;
     height: 35px;
     width: 100%;
     text-align: center;
-
     span:first-child {
        margin-right: 20px;
     }
   }
 }
-
 .card.active {
   z-index: 100;
   margin: 0 auto;
-
   .cover {
     height: 350px;
   }
-
   .card-text {
     height: auto;
     max-height: 200px;
   }
-
     @media only screen and (max-width: 514px){
       .card-text{
         height: auto;
         max-height: 300px;
       }
     }
-
     @media only screen and (min-width: 576px) {
       width: 500px;
       margin: 0 auto;
     }
-
     @media only screen and (min-width: 766px){
       width: 620px;
       margin: 0 15%;
     }
-
     @media only screen and (min-width: 960px){
       width: 620px;
       margin: 0 33%;
     }
-
     @media only screen and (min-width: 1264px) {
       width: 750px;
       margin: 0 60%;
     }
-
     @media only screen and (min-width: 1200px) and (max-width: 1264px){
       width: 700px;
-      margin: 0 40%;
+      margin: 0 50%;
     }
 }
-
 .card.inactive {
   z-index: -1;
 }
-
 // Alignment of active cards
 @media only screen and (min-width: 576px) {
   .col-md-6:nth-child(2n) .active {
@@ -241,7 +434,6 @@ span.highlighted {
     margin: 0 70%;
   }
 }
-
 .row.active {
   background: rgba(255, 255, 255, 0.8);
 }
